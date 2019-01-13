21b77f56b286045575e25f987cd361d70768f1dd
michalbogaczynski <bogaczynski.m@gmail.com>
2019-01-13 21:09:30 +0100

    Definiowanie wlasciwosci

---
+++ Kurs.BL/Klient.cs
@@ -0,0 +1,35 @@
+﻿namespace Kurs.BL
+{
+    public class Klient
+    {
+        private string _nazwisko;
+        public string Nazwisko
+        {
+            get
+            { 
+                // tutaj możemy dodać dowlony kod
+                return _nazwisko;
+            }
+            set
+            {
+                // tutaj możemy dodać dowlony kod
+                _nazwisko = value;
+            }
+        }
+
+        public string Imie { get; set; }
+
+        public string Email { get; set; }
+
+        public int KlientId { get; private set; }
+
+        public string ImieNazwiskoP
+        {
+            get
+            {
+                return Imie + "," + Nazwisko;
+            }
+        }
+        
+    }
+}
